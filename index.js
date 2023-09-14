1d1ada1c85ff897328674a27ae2ecec3c8da587e
almaaqila <almaaqila@gmail.com>
2023-09-14 21:02:33 +0700

    push todolist app

---
+++ index.js
@@ -0,0 +1,27 @@
+//1. reference html element form in js
+const formTask = document.querySelector(".form-task");
+//contoh lain pakai const formTask = document.getElementsByClassName(".form-task");
+
+
+//2. menambahkan event listener du element form
+formTask.addEventListener("submit", (e) => { // tiap event ada parameter e = event
+    e.preventDefault();
+    
+    //3. referene html element input 
+    const inputForm = document.querySelector(".input-form");   
+    console.log(inputForm.value);
+
+    //4. reference html element ul / wrapper list
+    const wrapperList = document.querySelector(".task-list-wrapper")
+
+    //5. create document <li> using js
+    const taskList = document.createElement("li");
+
+    //6. masukkan input value ke <li>
+    taskList.innerHTML = inputForm.value;
+    //<li>inputvalue</li>
+
+    //7. append <li> ke <ul>
+    wrapperList.append(taskList);
+});
+
